c8c837b3ec1b4bbcc050ce3ffd9ea1a4dd6ef32c
Frode Johannesen <fjohannesen@openpipe.berlios.de>
2007-10-23 16:42:46 +0000

    Added parsers for OpenOffice documents. Added tests for 3 document types: .odp, .ods and .odt

---
+++ parse-misc/src/main/java/no/trank/openpipe/parse/oo/OOParser.java
@@ -0,0 +1,175 @@
+package no.trank.openpipe.parse.oo;
+
+import java.io.Closeable;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+
+import no.trank.openpipe.parse.api.ParseData;
+import no.trank.openpipe.parse.api.Parser;
+import no.trank.openpipe.parse.api.ParserException;
+import no.trank.openpipe.parse.api.ParserResult;
+import no.trank.openpipe.parse.api.ParserResultImpl;
+
+/**
+ * Parser for OpenOffice documents.
+ * 
+ * @version $Revision$
+ */
+public class OOParser implements Parser, Closeable {
+
+   public ParserResult parse(ParseData data) throws IOException, ParserException {
+      ZipInputStream inputStream = new ZipInputStream(data.getInputStream());
+
+      String text = null;
+      Map<String, String> properties = new HashMap<String, String>();
+
+      ZipEntry ze = null;
+      while ((ze = inputStream.getNextEntry()) != null) {
+         if (ze.getName().equals("content.xml")) {
+            try {
+               text = parseContent(inputStream);
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
+         } else if (ze.getName().equals("meta.xml")) {
+            try {
+               parseMeta(inputStream, properties);
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
+         }
+      }
+
+      final ParserResultImpl result = new ParserResultImpl();
+      result.setText(text);
+      result.setTitle(properties.get("title"));
+      if (data.includeProperties()) {
+         result.setProperties(properties);
+      }
+      return result;
+   }
+
+   public void close() throws IOException {
+   }
+
+   private static String parseContent(ZipInputStream zis) throws Exception {
+      StringBuilder sb = new StringBuilder();
+
+      XMLStreamReader reader = null;
+
+      try {
+         // exhaust the stream, but don't close it
+         reader = XMLInputFactory.newInstance().createXMLStreamReader(
+               new FilterInputStream(zis) {
+                  public void close() {
+                  };
+               });
+
+         boolean lastp = true;
+         String inTag = null;
+         String link = null;
+
+         while (reader.hasNext()) {
+            reader.next();
+            if (reader.isStartElement()) {
+               if ("p".equals(reader.getLocalName())) {
+                  inTag = "p";
+               } else if (reader.getLocalName().equals("a")) {
+                  link = reader.getAttributeValue("http://www.w3.org/1999/xlink", "href");
+                  inTag = "a";
+               } else if (reader.getLocalName().startsWith("tab")) {
+                  inTag = "tab";
+               } else if (reader.getLocalName().equals("span")) {
+                  inTag = "span";
+               } else {
+                  inTag = null;
+               }
+            } else if (reader.isCharacters() && inTag != null) {
+               String text = reader.getText();
+
+               if ("p".equals(inTag) && text.length() > 0) {
+                  if (!lastp) {
+                     sb.append('\n');
+                  }
+                  sb.append(text).append('\n');
+                  lastp = true;
+               } else if ("tab".equals(inTag)) {
+                  sb.append("\t");
+                  lastp = false;
+               } else if ("a".equals(inTag)) {
+                  sb.append(text).append(' ').append(link);
+                  lastp = false;
+               } else if ("span".equals(inTag) && text.length() > 0) {
+                  if (!lastp) {
+                     sb.append(' ');
+                  }
+                  sb.append(text);
+                  lastp = false;
+               }
+            }
+         }
+      } finally {
+         if (reader != null) {
+            try {
+               reader.close();
+            } catch (Exception e) {
+            }
+         }
+      }
+
+      return sb.toString();
+   }
+
+   private static void parseMeta(ZipInputStream zis, Map<String, String> properties) throws Exception {
+      XMLStreamReader reader = null;
+
+      try {
+         // exhaust the stream, but don't close it
+         reader = XMLInputFactory.newInstance().createXMLStreamReader(
+               new FilterInputStream(zis) {
+                  public void close() {
+                  };
+               });
+
+         String inTag = null;
+
+         while (reader.hasNext()) {
+            reader.next();
+            if (reader.isStartElement()) {
+               inTag = reader.getLocalName();
+            } else if (reader.isCharacters()) {
+               String text = reader.getText();
+               if (text.length() > 0) {
+                  if ("title".equals(inTag)) {
+                     properties.put("title", text);
+                  } else if ("language".equals(inTag)) {
+                     properties.put("language", text);
+                  } else if ("creation-date".equals(inTag)) {
+                     properties.put("creation-date", text);
+                  } else if ("print-date".equals(inTag)) {
+                     properties.put("print-date", text);
+                  } else if ("generator".equals(inTag)) {
+                     properties.put("generator", text);
+                  } else if ("creator".equals(inTag)) {
+                     properties.put("creator", text);
+                  }
+               }
+            }
+         }
+      } finally {
+         if (reader != null) {
+            try {
+               reader.close();
+            } catch (Exception e) {
+            }
+         }
+      }
+   }
+}
---
+++ parse-misc/src/test/java/no/trank/openpipe/parse/oo/OOParserTest.java
@@ -0,0 +1,82 @@
+package no.trank.openpipe.parse.oo;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import junit.framework.TestCase;
+import no.trank.openpipe.parse.api.ParseData;
+import no.trank.openpipe.parse.api.ParserResult;
+
+/**
+ * @version $Revision$
+ */
+public class OOParserTest extends TestCase {
+
+   public void testParseODP() throws Exception {
+      final OOParser parser = new OOParser();
+      final ParserResult result = parser.parse(new TestData("test.odp"));
+      final String text = result.getText();
+      
+      assertNotNull(text);
+      assertEquals(true, text.contains("Slide 1"));
+      assertEquals(true, text.contains("Slide 2"));
+      assertEquals(true, text.contains("Text on slide 2"));  
+      
+      assertEquals("Frode Johannesen", result.getProperties().get("creator"));
+   }
+   
+   public void testParseODS() throws Exception {
+      final OOParser parser = new OOParser();
+      final ParserResult result = parser.parse(new TestData("test.ods"));
+      final String text = result.getText();
+      
+      assertNotNull(text);
+      assertEquals(true, text.contains("Cell text"));
+      assertEquals(true, text.contains("4"));
+      assertEquals(true, text.contains("Sheet 2 text"));  
+      
+      assertEquals("Frode Johannesen", result.getProperties().get("creator"));
+   }
+   
+   public void testParseODT() throws Exception {
+      final OOParser parser = new OOParser();
+      final ParserResult result = parser.parse(new TestData("test.odt"));
+      final String text = result.getText();
+      
+      assertNotNull(text);
+      assertEquals(true, text.contains("Some text"));
+      assertEquals(true, text.contains("Text"));
+      assertEquals(true, text.contains("in"));  
+      assertEquals(true, text.contains("a"));
+      assertEquals(true, text.contains("table"));
+      assertEquals(true, text.contains("Link http://link.link.link/"));
+      
+      assertEquals("Frode Johannesen", result.getProperties().get("creator"));
+   }
+
+   private static class TestData implements ParseData {
+      private final String fileName;
+      
+      TestData(String fileName) {
+         this.fileName = fileName;
+      }
+      
+      public InputStream getInputStream() throws IOException {
+         final InputStream in = getClass().getResourceAsStream("/oo/" + fileName);
+         assertNotNull(in);
+         return in;
+      }
+
+      public int getLength() {
+         return 0;
+      }
+
+      public boolean includeProperties() {
+         return true;
+      }
+
+      public String getFileName() {
+         return fileName;
+      }
+   }
+}
