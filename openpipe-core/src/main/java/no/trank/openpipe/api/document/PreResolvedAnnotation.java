b36db70961bd7cafa104f8cec62c7e6406eda7f0
Espen Amble Kolstad <espenamblekolstad@gmail.com>
2007-10-24 07:35:53 +0000

    Added PreResolvedAnnotation

---
+++ openpipe-core/src/main/java/no/trank/openpipe/api/document/PreResolvedAnnotation.java
@@ -0,0 +1,33 @@
+package no.trank.openpipe.api.document;
+
+/**
+ * A basic implementation of {@link ResolvedAnnotation} with a pre-resolved value.
+ * 
+ * @version $Revision$
+ */
+public class PreResolvedAnnotation extends BaseAnnotation implements ResolvedAnnotation {
+   private final String value;
+
+   /**
+    * Constructs a <tt>PreResolvedAnnotation</tt> with a given pre-resolved value and <tt>startPos = endPos = 0</tt>.
+    * 
+    * @param value the pre-resolved value.
+    */
+   public PreResolvedAnnotation(String value) {
+      this(0, 0, value);
+   }
+
+   /**
+    * Constructs a <tt>PreResolvedAnnotation</tt> with a given pre-resolved value.
+    * 
+    * @param value the pre-resolved value.
+    */
+   public PreResolvedAnnotation(int startPos, int endPos, String value) {
+      super(startPos, endPos);
+      this.value = value;
+   }
+
+   public String getValue() {
+      return value;
+   }
+}
