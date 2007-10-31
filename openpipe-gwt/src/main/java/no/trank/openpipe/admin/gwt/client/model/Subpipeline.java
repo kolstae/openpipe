f827234331431b49a1b7495d9edb1abbd4514a97
Frode Johannesen <fjohannesen@openpipe.berlios.de>
2007-10-31 12:40:55 +0000

    Took openpipe-gwt out for now. Will be re-commited when it's more mature.

--- openpipe-gwt/src/main/java/no/trank/openpipe/admin/gwt/client/model/Subpipeline.java
+++
@@ -1,7 +0,0 @@
-package no.trank.openpipe.admin.gwt.client.model;
-
-public class Subpipeline {
-   public Subpipeline(String prefix, Object value) {
-      System.out.println(prefix);
-   }
-}
--- openpipe-gwt/src/main/java/no/trank/openpipe/admin/gwt/client/model/value/ValueWrapper.java
+++
@@ -1,9 +0,0 @@
-package no.trank.openpipe.admin.gwt.client.model.value;
-
-/**
- * @version $Revision$
- */
-public interface ValueWrapper {
-   Object getValue();
-   Object setValue(Object value);
-}
