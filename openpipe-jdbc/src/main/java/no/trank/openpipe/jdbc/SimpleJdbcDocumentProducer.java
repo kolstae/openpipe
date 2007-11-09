f11366e3611948b0224abad3eeabf87061a04301
Geir H. Pettersen <geir@openpipe.berlios.de>
2007-11-09 08:31:09 +0000

    Added javadoc

--- openpipe-jdbc/src/main/java/no/trank/openpipe/jdbc/SimpleJdbcDocumentProducer.java
+++ openpipe-jdbc/src/main/java/no/trank/openpipe/jdbc/SimpleJdbcDocumentProducer.java
@@ -29,6 +29,25 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import static no.trank.openpipe.api.document.DocumentOperation.*;
 
 /**
+ * A simple document producer for getting documents from a jdbc source.
+ *
+ * The producer can trigger add, modify and/or deletes. Each operation has a corresponding pre, post, fail and list
+ * sqls. Ex: for add you have the setters: setAddPreSql(...), setAddSql(...), setAddPostSql(...) and
+ * setAddFailSql(List<String> sql).
+ *
+ * For each operation type (add, modify, delete):<br/>
+ * - The preSql is run<br/>
+ * - Each document is produced by running the producer sql. There is no guarantee that all documents are fetched before
+ * next step happens. But it is guaranteed that a after a post/failSql a new preSql is run before new documents are
+ * fetched.<br/>
+ * - If no errors happened the postSql is run.</br>
+ * - If an error happened the failSql is run.</br>
+ *
+ * To use:
+ *
+ * 1. Set the JdbcTemplate (and its dataSource)</br>
+ * 2. Set the sqls for producing documents.</br>
+ *
  * @version $Revision$
  */
 public class SimpleJdbcDocumentProducer extends JdbcDocumentProducer {
@@ -61,66 +80,166 @@ public class SimpleJdbcDocumentProducer extends JdbcDocumentProducer {
       return part;
    }
 
+   /**
+    * Set a list of preSqls to run.
+    *
+    * @param operation the operation that this preSql is used for.
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    public void setPreSql(String operation, List<String> sql) {
       getPart(operation).setPreSql(sql);
    }
 
+   /**
+    * Set a list of sqls to run to get the documents.
+    *
+    * @param operation the operation that this preSql is used for.
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    public void setSql(String operation, List<String> sql) {
       getPart(operation).setSqls(sql);
    }
 
+   /**
+    * Set a list of sqls to run after documents have been fetched.
+    *
+    * @param operation the operation that this preSql is used for.
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    public void setPostSql(String operation, List<String> sql) {
       getPart(operation).setPostSql(sql);
    }
 
+   /**
+    * Set a list of sqls to run if there was an error in the pipeline.
+    *
+    * @param operation the operation that this preSql is used for.
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    private void setFailSql(String operation, List<String> sql) {
       getPart(operation).setFailSql(sql);      
    }
 
+   /**
+    * Set a list of preSqls to run before producing add Documents.
+    *
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    public void setAddPreSql(List<String> sql) {
       setPreSql(ADD_VALUE, sql);
    }
 
+   /**
+    * Set a list of sqls to run for producing add Documents.
+    *
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    public void setAddSql(List<String> sql) {
       setSql(ADD_VALUE, sql);
    }
 
+   /**
+    * Set a list of sqls to run after add documents have been produced. (If no error happened)
+    *
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    public void setAddPostSql(List<String> sql) {
       setPostSql(ADD_VALUE, sql);
    }
 
+   /**
+    * Set a list of sqls to run after add documents have been produced and some error happened.
+    *
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    public void setAddFailSql(List<String> sql) {
       setFailSql(ADD_VALUE, sql);
    }
 
+   /**
+    * Set a list of preSqls to run before producing modify Documents.
+    *
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    public void setModifyPreSql(List<String> sql) {
       setPreSql(MODIFY_VALUE, sql);
    }
 
+   /**
+    * Set a list of sqls to run for producing modify Documents.
+    *
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    public void setModifySql(List<String> sql) {
       setSql(MODIFY_VALUE, sql);
    }
 
+   /**
+    * Set a list of sqls to run after modify documents have been produced. (If no error happened)
+    *
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    public void setModifyPostSql(List<String> sql) {
       setPostSql(MODIFY_VALUE, sql);
    }
 
+   /**
+    * Set a list of sqls to run after modify documents have been produced and some error happened.
+    *
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    public void setModifyFailSql(List<String> sql) {
       setFailSql(MODIFY_VALUE, sql);
    }
 
+   /**
+    * Set a list of preSqls to run before producing delete Documents.
+    *
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    public void setDeletePreSql(List<String> sql) {
       setPreSql(DELETE_VALUE, sql);
    }
 
+   /**
+    * Set a list of sqls to run for producing delete Documents.
+    *
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    public void setDeleteSql(List<String> sql) {
       setSql(DELETE_VALUE, sql);
    }
 
+   /**
+    * Set a list of sqls to run after delete documents have been produced. (If no error happened)
+    *
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */
    public void setDeletePostSql(List<String> sql) {
       setPostSql(DELETE_VALUE, sql);
    }
 
+   /**
+    * Set a list of sqls to run after delete documents have been produced and some error happened.
+    *
+    * @param sql a list of sql to run as preSql
+    * @see no.trank.openpipe.api.document.DocumentOperation
+    */      
    public void setDeleteFailSql(List<String> sql) {
       setFailSql(DELETE_VALUE, sql);
    }
