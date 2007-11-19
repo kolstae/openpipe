d3a58545d28921ef6e4e254e8920f2b6c605338b
Frode Johannesen <fjohannesen@openpipe.berlios.de>
2007-11-19 20:16:21 +0000

    javadoc

--- openpipe-core/src/main/java/no/trank/openpipe/api/PipelineRunner.java
+++ openpipe-core/src/main/java/no/trank/openpipe/api/PipelineRunner.java
@@ -15,27 +15,54 @@
  */
 package no.trank.openpipe.api;
 
+import no.trank.openpipe.api.document.Document;
 import no.trank.openpipe.api.document.DocumentProducer;
 
 /**
+ * A <code>PipelineRunner</code> instance requests {@link Document}s from a {@link DocumentProducer} and feeds
+ * them into a {@link Pipeline}.
+ * 
+ * <p>If the {@link DocumentProducer} implements {@link PipelineExceptionListener}, it is added to the
+ * {@link Pipeline}'s {@link PipelineExceptionHandler} before feeding the {@link Document}s, and removed after.    
+ * 
  * @version $Revision$
  */
 public class PipelineRunner implements Runnable {
    private Pipeline pipeline;
    private DocumentProducer documentProducer;
 
+   /**
+    * Gets the pipeline that the {@link Document}s are fed into.
+    * 
+    * @return the pipeline
+    */
    public Pipeline getPipeline() {
       return pipeline;
    }
 
+   /**
+    * Sets the pipeline that the {@link Document}s are fed into.
+    * 
+    * @param pipeline the pipeline
+    */
    public void setPipeline(Pipeline pipeline) {
       this.pipeline = pipeline;
    }
 
+   /**
+    * Gets the {@link DocumentProducer} that produces the {@link Document}s.
+    * 
+    * @return the document producer
+    */
    public DocumentProducer getDocumentProducer() {
       return documentProducer;
    }
 
+   /**
+    * Sets the {@link DocumentProducer} that produces the {@link Document}s.
+    * 
+    * @param documentProducer the document producer
+    */
    public void setDocumentReader(DocumentProducer documentProducer) {
       this.documentProducer = documentProducer;
    }
@@ -65,7 +92,6 @@ public class PipelineRunner implements Runnable {
          } else {
             documentProducer.fail();
          }
-
       }
    }
 }
